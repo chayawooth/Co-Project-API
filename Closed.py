7bbbbb67a450e9b39f5ce8bc6cfc437033569569
chayawooth <chayawooth.puy@gmail.com>
2022-06-08 10:37:28 +0700

    change load

--- Closed.py
+++ Closed.py
@@ -5,6 +5,8 @@ import datetime
 
 try:
     ticketNum=sys.argv[1]
+    cause=sys.argv[2]
+    subcause=sys.argv[3]
     times=datetime.datetime.now() - datetime.timedelta(seconds=3)
     nows = datetime.datetime.now()
     reportdate=times.strftime("%Y-%m-%dT%H:%M:%S")
@@ -14,13 +16,13 @@ try:
     headers = { 'Content-type' : 'application/json' , "Content-Encoding" : "gzip, deflate, br" }
 
     payload = json.dumps({
-        "wonum": "A4660701",
+        "ticketid": ticketNum,
+        "status": "RESOLVED",
         "externalsystem": "CFM",
-        "status": "COMP",
-        "updateby": "01028197",
+        "updatedby": "01063472",
         "failurecode": "MSOM",
-        "problemcode": "MS014",
-        "causecode": "MS0037",
+        "problemcode": cause,
+        "causecode": subcause,
         "remedycode": "MS01",
         "restorationdate": nows
     })
@@ -33,6 +35,7 @@ try:
     #print(response.headers)
     print(response.text)
 except:
-    print("please input subject longdescription")
+    print("please ticketNum  cause subcause ")
+    print("TcticketNum MS014 MS0037")
 
-print("Process Done!!")
+print("Closed Done!!")
