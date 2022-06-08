66a3df8c136505e69c86add9006ef92f05f3b9ff
chayawooth <chayawooth.puy@gmail.com>
2022-06-08 10:54:02 +0700

    change load

--- Closed.py
+++ Closed.py
@@ -4,16 +4,22 @@ import sys
 import datetime
 
 try:
-    ticketNum=sys.argv[1]
-    cause=sys.argv[2]
-    subcause=sys.argv[3]
-    times=datetime.datetime.now() - datetime.timedelta(seconds=3)
+    ticketNum = sys.argv[1]
+    cause = sys.argv[2]
+    subcause = sys.argv[3]
+
+    print("ticketNum :", ticketNum)
+    print("cause :", cause)
+    print("subcause :", subcause)
+
+    times = datetime.datetime.now() - datetime.timedelta(seconds=3)
     nows = datetime.datetime.now()
-    reportdate=times.strftime("%Y-%m-%dT%H:%M:%S")
-    affecteddate=times.strftime("%Y-%m-%dT%H:%M:%S")
+    reportdate = times.strftime("%Y-%m-%dT%H:%M:%S")
+    affecteddate = times.strftime("%Y-%m-%dT%H:%M:%S")
 
     url = "http://10.50.90.202:8080/nttservice/msom-ticket"
-    headers = { 'Content-type' : 'application/json' , "Content-Encoding" : "gzip, deflate, br" }
+    headers = {'Content-type': 'application/json',
+               "Content-Encoding": "gzip, deflate, br"}
 
     payload = json.dumps({
         "ticketid": ticketNum,
@@ -26,14 +32,14 @@ try:
         "remedycode": "MS01",
         "restorationdate": nows
     })
-
     print("Now time : ", nows)
     print("New time : ", times)
     print(payload)
-    response = requests.request("POST", url, headers=headers, data=payload,auth = ("Chayawo" , "cdexswzaQ*01028197"))
+    response = requests.request("patch", url, headers=headers, data=payload, auth=("Chayawo", "cdexswzaQ*01028197"))
     print(requests.get(url).headers)
-    #print(response.headers)
+    # print(response.headers)
     print(response.text)
+
 except:
     print("please ticketNum  cause subcause ")
     print("TcticketNum MS014 MS0037")
