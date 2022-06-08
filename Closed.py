65a2b18055b28e6f00560b8a35626f110c6406f3
chayawooth <chayawooth.puy@gmail.com>
2022-06-08 11:05:50 +0700

    change load

--- Closed.py
+++ Closed.py
@@ -7,39 +7,32 @@ try:
     ticketNum = sys.argv[1]
     cause = sys.argv[2]
     subcause = sys.argv[3]
-
     print("ticketNum :", ticketNum)
     print("cause :", cause)
     print("subcause :", subcause)
-
     times = datetime.datetime.now() - datetime.timedelta(seconds=3)
     nows = datetime.datetime.now()
+    print("Now time : ", nows)
+    print("New time : ", times)
     reportdate = times.strftime("%Y-%m-%dT%H:%M:%S")
     affecteddate = times.strftime("%Y-%m-%dT%H:%M:%S")
-
     url = "http://10.50.90.202:8080/nttservice/msom-ticket"
-    headers = {'Content-type': 'application/json',
-               "Content-Encoding": "gzip, deflate, br"}
-
+    headers = {'Content-type': 'application/json',"Content-Encoding": "gzip, deflate, br"}
     payload = json.dumps({
         "ticketid": ticketNum,
         "status": "RESOLVED",
         "externalsystem": "CFM",
-        "updatedby": "01063472",
+        "updatedby": "01028197",
         "failurecode": "MSOM",
         "problemcode": cause,
         "causecode": subcause,
         "remedycode": "MS01",
-        "restorationdate": nows
-    })
-    print("Now time : ", nows)
-    print("New time : ", times)
+        "restorationdate": reportdate })
     print(payload)
-    response = requests.request("patch", url, headers=headers, data=payload, auth=("Chayawo", "cdexswzaQ*01028197"))
+    response = requests.request("PATCH", url, headers=headers, data=payload, auth=("Chayawo", "cdexswzaQ*01028197"))
     print(requests.get(url).headers)
     # print(response.headers)
     print(response.text)
-
 except:
     print("please ticketNum  cause subcause ")
     print("TcticketNum MS014 MS0037")
