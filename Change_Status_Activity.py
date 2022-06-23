b31c4e8d88b73a1108945ffa88db98f1027a1e55
chayawooth <chayawooth.puy@gmail.com>
2022-06-23 14:23:03 +0700

    add Complete_Activityate

---
+++ Change_Status_Activity.py
@@ -0,0 +1,30 @@
+import requests
+import json
+import sys
+import datetime
+from configure import *
+try:
+    wonumber=sys.argv[1]
+    times=datetime.datetime.now() - datetime.timedelta(seconds=3)
+    nows = datetime.datetime.now()
+    reportdate=times.strftime("%Y-%m-%dT%H:%M:%S")
+    affecteddate=times.strftime("%Y-%m-%dT%H:%M:%S")
+
+    url = "http://10.50.90.202:8080/nttservice/msom-ticket"
+    headers = {'Content-type': 'application/json'}
+    payload = json.dumps({ "wonum" : wonumber ,
+                                        "externalsystem" : "CFM" ,
+                                        "status" : "INPRG" ,
+                                        "updateby" : "01063472"
+                           } )
+    print("Now time : ", nows)
+    print("New time : ", times)
+    print(payload)
+    response = requests.request("patch", url, headers=headers, data=payload,auth = ( user , pwd ))
+    print(requests.get(url).headers)
+    #print(response.headers)
+    print(response.text)
+except:
+    print("please input wonumber ")
+
+print("Process Done!!")
