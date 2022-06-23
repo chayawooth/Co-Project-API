7ec69d37bd94064ae2e9f0b7c6a009edf8878586
chayawooth <chayawooth.puy@gmail.com>
2022-06-23 14:08:01 +0700

    add Add_Worklog.py

---
+++ Add_Worklog.py
@@ -0,0 +1,31 @@
+import requests
+import json
+import sys
+import datetime
+from configure import *
+try:
+    ticketid=sys.argv[1]
+    worklog=sys.argv[2]
+    times=datetime.datetime.now() - datetime.timedelta(seconds=3)
+    nows = datetime.datetime.now()
+    reportdate=times.strftime("%Y-%m-%dT%H:%M:%S")
+    affecteddate=times.strftime("%Y-%m-%dT%H:%M:%S")
+
+    url = "http://10.50.90.202:8080/nttservice/msom-ticket"
+    headers = {'Content-type': 'application/json'}
+    payload = json.dumps({ { "createby" : "01063472" ,
+                                       "recordkey" : ticketid ,
+                                       "subject" : "Update log" + ticketid ,
+                                       "longdescription" : worklog
+                                       }} )
+    print("Now time : ", nows)
+    print("New time : ", times)
+    print(payload)
+    response = requests.request("post", url, headers=headers, data=payload,auth = ( user , pwd ))
+    print(requests.get(url).headers)
+    #print(response.headers)
+    print(response.text)
+except:
+    print("please input ticketid  Worklog ")
+
+print("Process Done!!")
