182580b770667699bc028effe18961f06e682a3d
chayawooth <chayawooth.puy@gmail.com>
2022-09-11 22:03:18 +0700

    send to CFM but not work

--- Inquiry_Ticket.py
+++ Inquiry_Ticket.py
@@ -1,44 +1,47 @@
 import requests
 import sys
+from configure import *
 
 
-url ="http://10.50.90.202:8080/nttservice/msom-ticket?"
+#url ="http://10.50.90.202:8080/nttservice/msom-ticket?"
+url =ipaddressProduct+"/nttservice/msom-ticket?"
+headers = {'Content-type': 'application/json'}
 try:
     ticket_num=sys.argv[1]
     ticketid = "ticketid=" + ticket_num
-    print(ticketid)
-    response = requests.get(url + ticketid)
+    #print(ticketid)
+    response = requests.get(url + ticketid,headers=headers,auth=(user , pwd))
 
     #Result
 
-    print(response.json()['msomticketlist'][0])
+    #print(response.json()['msomticketlist'][0])
 
     print("####### TT Inquiry ######")
     print("ticketid : ", response.json()['msomticketlist'][0]['ticketid'])
-    print("tickettype : ",response.json()['msomticketlist'][0]['tickettype'])
+    #print("tickettype : ",response.json()['msomticketlist'][0]['tickettype'])
     print("status : ",response.json()['msomticketlist'][0]['status'])
     print("subject : ",response.json()['msomticketlist'][0]['subject'])
     print("longdescription : ",response.json()['msomticketlist'][0]['longdescription'])
     print("classification : ",response.json()['msomticketlist'][0]['classification'])
-    print("severity : ",response.json()['msomticketlist'][0]['severity'])
-    print("urgency : ",response.json()['msomticketlist'][0]['urgency'])
-    print("slaresolutiontime : ",response.json()['msomticketlist'][0]['slaresolutiontime'])
-    print("owner : ",response.json()['msomticketlist'][0]['owner'])
+    #print("severity : ",response.json()['msomticketlist'][0]['severity'])
+    #print("urgency : ",response.json()['msomticketlist'][0]['urgency'])
+    #print("slaresolutiontime : ",response.json()['msomticketlist'][0]['slaresolutiontime'])
+    #print("owner : ",response.json()['msomticketlist'][0]['owner'])
     print("ownergroup : ",response.json()['msomticketlist'][0]['ownergroup'])
     print("createdby : ",response.json()['msomticketlist'][0]['createdby'])
-    print("createdbyname : ",response.json()['msomticketlist'][0]['createdbyname'])
-    print("reportedby : ",response.json()['msomticketlist'][0]['reportedby'])
-    print("reportedbyname : ",response.json()['msomticketlist'][0]['reportedbyname'])
-    print("externalsystem : ",response.json()['msomticketlist'][0]['externalsystem'])
-    print("externalsystemuser : ",response.json()['msomticketlist'][0]['externalsystemuser'])
+    #print("createdbyname : ",response.json()['msomticketlist'][0]['createdbyname'])
+    #print("reportedby : ",response.json()['msomticketlist'][0]['reportedby'])
+    #print("reportedbyname : ",response.json()['msomticketlist'][0]['reportedbyname'])
+    #print("externalsystem : ",response.json()['msomticketlist'][0]['externalsystem'])
+    #print("externalsystemuser : ",response.json()['msomticketlist'][0]['externalsystemuser'])
     print("reportdate : ",response.json()['msomticketlist'][0]['reportdate'])
-    print("affecteddate : ",response.json()['msomticketlist'][0]['affecteddate'])
-    print("creationdate : ",response.json()['msomticketlist'][0]['creationdate'])
-    print("restorationdate : ",response.json()['msomticketlist'][0]['restorationdate'])
-    print("externalsystemuser : ",response.json()['msomticketlist'][0]['externalsystemuser'])
-    print("wonum : ",response.json()['msomticketlist'][0]['msomactivitylist'][2]['wonum'])
-    print("########### End ############ chayawooth")
+    #print("affecteddate : ",response.json()['msomticketlist'][0]['affecteddate'])
+    #print("creationdate : ",response.json()['msomticketlist'][0]['creationdate'])
+    #print("restorationdate : ",response.json()['msomticketlist'][0]['restorationdate'])
+    #print("externalsystemuser : ",response.json()['msomticketlist'][0]['externalsystemuser'])
+    #print("wonum : ",response.json()['msomticketlist'][0]['msomactivitylist'][2]['wonum'])
+    print("########### End #############")
 except:
     print("pls input ticket number")
 
-print("Response :",response.json())
+#print("Response :",response.json())
--- configure.py
+++ configure.py
@@ -3,10 +3,10 @@
 user="Chayawo"
 pwd="cdexswzaQ*01028197"
 
-
+ipaddressProduct="http://10.50.90.101:8080" ## production
+ipaddressPB="http://10.50.90.202:8080" ## TB
 user_ice = "Wallop2"
 password_ice = "Wa!!0p@chan2022"
-
 ## MSOMSERV
 Username = "MSOMSERV"
 Password = "P@$$_Ms0M_ScCD-2022_SeRver@TrUe)-("
